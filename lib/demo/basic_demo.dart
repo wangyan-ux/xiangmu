426b1a31aadaad82c61c1e26b728aeef63ba10d6
wangyan <893981280@qq.com>
2020-01-09 17:52:43 +0800

    横排显示（comtainer)

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -5,30 +5,26 @@ class BasicDemo extends  StatelessWidget{
   @override
   Widget build(BuildContext context) {
    
-return RichText(
-  text: TextSpan(
-    text: 'NIHAONG',
-    style: TextStyle(
-       fontSize: 16.0,
-      color:Colors.black,
-      fontWeight: FontWeight.bold,
-      fontStyle: FontStyle.italic
-      
+return Container(
+color:Colors.grey[100],
+child: Row(
+  children: <Widget>[
+    Container(
+      child: Icon(Icons.pool,size: 32.0,color: Colors.white,),
+      padding: EdgeInsets.all(8.0),
+      color: Color.fromRGBO(3, 54, 255, 1.0),
+      margin: EdgeInsets.all(5.0)
     ),
-    children: [
-      TextSpan(
-        text: 'wangyan',
-        style: TextStyle(
-          fontSize: 16.0,
-          fontWeight:FontWeight.bold,
-          fontStyle: FontStyle.normal,
-          color: Colors.blue
-        )
-      )
-    ]
-      
-    
-  ),
+    Container(
+      child: Icon(Icons.pool,size: 32.0,color: Colors.white,),
+      padding: EdgeInsets.all(8.0),
+      color: Color.fromRGBO(3, 54, 255, 1.0),
+      margin: EdgeInsets.all(5.0)
+    )
+  ],
+),
+
 );
+
   }
 }
